f1cf76435082ce2b6da45e518269d11c425efe40
MirceaOvidiu <mircea.ovidiu.03@gmail.com>
2023-11-14 22:16:50 +0200

    added transparent header and footer

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 function goBack() {
     window.history.back();
 }
+
+var mybutton = document.getElementById("myBtn");
+
+// when the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// scroll to the top of the document on button press
+mybutton.onclick = function() {topFunction()};
+
+function topFunction() {
+  document.body.scrollTop = 0; //Safari
+  document.documentElement.scrollTop = 0; //Chrome, Firefox
+}
